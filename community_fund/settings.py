55949bbdf72517ff760b9dc1fabcd0431b963a2c
Kaleab Zewdie <kalzed1921@gmail.com>
2025-05-24 23:24:54 +0300

    adding important libraries to project

--- community_fund/settings.py
+++ community_fund/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c8syymns8avox!_2$&58%eg!g)v@o8yx!vbdik30sq%9z6irtx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
